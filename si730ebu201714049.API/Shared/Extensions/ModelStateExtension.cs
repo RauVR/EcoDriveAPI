dc9666afa8bd0dc38b7a523b7c943e6f50ad9e3a
RauVR <u201714049@upc.edu.pe>
2022-11-29 18:55:19 -0500

    thirt commit

---
+++ si730ebu201714049.API/Shared/Extensions/ModelStateExtension.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace si730ebu201714049.API.Shared.Extensions;
+
+public static class ModelStateExtension
+{
+    public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+    {
+        return dictionary.SelectMany(m => m.Value.Errors)
+            .Select(m => m.ErrorMessage)
+            .ToList();
+    }
+}
